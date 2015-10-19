4ec1753a7deb9892b564f0547b8d74553e4194fa
Russell Kitchen <russellkitchen@live.com>
2015-10-19 16:38:50 +0100

    Add comments

--- application/controllers/temp.php
+++ application/controllers/temp.php
@@ -6,16 +6,22 @@ class Temp extends MY_Controller {
 	}
 
 	public function index() {
+		//activated boolean check, used in view
 		$this->data->activated = false;
 
+		//if activation code is not set, redirect
 		if (!$cid = $this->uri->segment(2)) {
 			redirect('404');
 		}
 		
 		//if code in uri matches an instance in the database, continue
 		if ($c_data = $this->model_temp->get_by(array('code'=>$cid), true)) {
+			//update user to be active
 			$this->model_users->save(array('active'=>1), $c_data->user_id);
+
+			//delete any other activation codes for said user
 			$this->model_temp->delete_by(array('user_id'=>$c_data->user_id));
+			
 			$this->data->activated = true;
 		} else {
 			$this->session->set_flashdata('error','<p>Invalid or expired activation link. Please try resending another by attempting to login.</p>');
--- application/controllers/thread.php
+++ application/controllers/thread.php
@@ -6,30 +6,38 @@ class Thread extends MY_Controller {
 	}
 
 	public function index() {
+		//if thread id is not set, redirect
 		if (!$tid = $this->uri->segment(2)) {
 			redirect('404');
 		}
 
+		//if thread is is not valid, redirect
 		if (!$this->model_threads->get($tid)) {
 			redirect('404');
 		}
 
+		//get page number from uri, if not set, 0
 		$page = ($this->uri->segment(3)) ? $this->uri->segment(3)-1 : 0;
+
+		//pagination function using: current url, post per page limit, location of page number by uri
+		//and what to search to get post count
 		$this->data->pagination = $this->model_posts->pagination(
 			'http://localhost/projects/11th/thread/'.$tid.'/', 
 			$this->post_limit,
 			3, 
 			array('thread_id'=>$tid));
 
+		//get posts for specific thread as well as relevant user info
 		$this->db->join('users', 'users.user_id = posts.user_id');
 		$this->db->where('thread_id', $tid);
 		$this->db->order_by('posted_on');
 		$this->db->limit($this->post_limit, $page*$this->post_limit);
 		$this->data->posts = $this->model_posts->get();
 
+		//set validation rules for quick reply
 		$this->form_validation->set_rules($this->model_posts->rules);
 
-
+		//if form passes validation, run
 		if ($this->form_validation->run()!=false) {
 
 			//if user is not logged in redirect to login page and set erro flashdata to that effect
@@ -37,6 +45,8 @@ class Thread extends MY_Controller {
 				$this->session->set_flashdata('error','<p class="error">You must be logged in to post</p>');
 				redirect('site/login','refresh');
 			}
+
+			//set post data in array for saving
 			$arr = array(
 				'user_id'=>$this->session->userdata('id'),
 				'thread_id'=>$tid,
@@ -82,22 +92,31 @@ class Thread extends MY_Controller {
 			redirect('site/login','refresh');
 		}
 
+		//set validation rules
 		$this->form_validation->set_rules($this->model_posts->rules);
 		$this->form_validation->set_rules($this->model_threads->rules);
 
+		//if form passes validation, run
 		if ($this->form_validation->run()!=false) {
+
+			//set thread data for saving
 			$arr = array(
 				'forum_id'=>$fid,
 				'user_id'=>$this->session->userdata('id'),
 				'subject'=>$this->input->post('subject')
 				);
+
+			//get thread id of new thread whilst saving
 			$tid = $this->model_threads->save($arr);
 
+			//set post data for saving
 			$arr = array(
 				'thread_id'=>$tid,
 				'user_id'=>$this->session->userdata('id'),
 				'message'=>$this->input->post('message')
 				);
+
+			//get post id of new first post whilst saving
 			$pid = $this->model_posts->save($arr);
 
 			redirect('thread/'.$tid, 'refresh');
