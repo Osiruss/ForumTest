ff8a181ca92721e65fd028471debbf9f7cfd1ffd
Russell Kitchen <russellkitchen@live.com>
2015-10-19 16:28:01 +0100

    Add comments

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -10,6 +10,8 @@ class Site extends MY_Controller {
 	}
 
 	public function home() {
+
+		//get latest three posts
 		$this->db->select('threads.subject, posts.*, users.username');
 		$this->db->join('users','users.user_id = posts.user_id');
 		$this->db->join('threads','threads.thread_id = posts.thread_id');
@@ -17,7 +19,7 @@ class Site extends MY_Controller {
 		$this->db->order_by('posted_on desc');
 		$this->data->posts = $this->model_posts->get();
 
-
+		//get time since post in reasonable formatting (3 hours ago, etc)
 		for ($i=0; $i < count($this->data->posts); $i++) { 
 			$obj = $this->model_posts->time_expired($this->data->posts[$i]->posted_on);
 			$this->data->posts[$i]->ago = reset($obj);
@@ -29,22 +31,33 @@ class Site extends MY_Controller {
 	}
 
 	public function login() {
+
+		//if user is logged in, redirect
 		if($this->model_users->loggedin()) {
 			redirect('forum','refresh');
 		}
 
+		//if resent button has been pressed, resend email activation
 		if($this->input->post('resend_activation')) {
 			$user_data = $this->model_users->get_by(array('username'=>$this->input->post('username')), true);
 			$this->model_temp->send_confirmation($user_data->email, $user_data->user_id);
 		}
 
+		//set validation rules, get from Model_Users
 		$this->form_validation->set_rules($this->model_users->rules);
 
+		//if form passes validation, run
 		if($this->form_validation->run()!=false) {
+			
+			//if users login is sucessful, continue
 			if($this->model_users->login()) {
+
+				//if flashdata redirect is set, redirect back to page user was previously  on
 				if ($this->session->flashdata('redirect')) {
 					redirect($this->session->flashdata('redirect'), 'refresh');
 				}
+
+				//else redirect to forum home
 				redirect('forum','refresh');
 			}
 		} else if (isset($_SERVER['HTTP_REFERER'])) {
@@ -75,13 +88,17 @@ class Site extends MY_Controller {
 		if ($this->model_users->loggedin()) {
 			redirect('site');
 		}
-			
+		
+		//form validation rules, including two callbacks
 		$this->form_validation->set_rules($this->model_users->reg_rules);
 		$this->form_validation->set_message('username_check','This username is already in use, please try another.');
 		$this->form_validation->set_message('email_check','This email is already in use, please try another.');
 		$this->form_validation->set_message('matches','The passwords do not match, please correct.');
 
+		//if form passes validation, run
 		if ($this->form_validation->run()!=false) {
+
+			//set userdata for saving
 			$arr = array(
 				'username'=>$this->input->post('username'),
 				'pass'=>$this->model_users->hash($this->input->post('password')),
@@ -107,10 +124,12 @@ class Site extends MY_Controller {
 	}
 
 	public function username_check() {
+		//using core model function, check if username is unique
 		return $this->model_users->unique(array('username'=>$this->input->post('username'))) ? true : false;
 	}
 
 	public function email_check() {
+		//using core model function, check if email is unique
 		return $this->model_users->unique(array('email'=>$this->input->post('email'))) ? true : false;
 	}
 
