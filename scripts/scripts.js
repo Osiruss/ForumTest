299e7149cad416ae69ba1f68b8300c6eefa7dd4c
Russell Kitchen <russellkitchen@live.com>
2015-10-20 18:03:15 +0100

    Clean of broken script. Stupidly posted data to register page where account info would be saved beffore submitting. Next time pure ajax file.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,29 +1,3 @@
-/*if ($('.register__form').length) {
-	var f = document.getElementsByClassName('register__form')[0],
-		v = document.getElementsByClassName('validation')[0],
-		inp = f.getElementsByTagName('input'),
-		u = document.getElementById('username'),
-		p = document.getElementById('password'),
-		p2 = document.getElementById('password2'),
-		em = document.getElementById('email');
-
-	for (var i = 0; i < inp.length; i++) {
-		inp[i].onkeyup = function() {
-
-			$.post('register', {
-				username: u.value, 
-				password: p.value, 
-				password2: p2.value, 
-				email: em.value
-			}, function(response, textStatus, xhr) {
-				//finds the validation class of current page
-				//adds html to said class by finding specific validation class from html response
-				$('.validation').html($(response).find('.validation').html());
-			});	
-		}
-	};
-}*/
-
 //unifinished form validation
 if ($('textarea[name=message]').length) {
 	var message = document.getElementsByTagName('textarea');
