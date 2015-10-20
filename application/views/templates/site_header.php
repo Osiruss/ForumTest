6f6454e27f8f548ba12ce19a4e08121bf688154d
Russell Kitchen <russellkitchen@live.com>
2015-10-20 17:44:02 +0100

    Add device-height

--- application/views/templates/site_header.php
+++ application/views/templates/site_header.php
@@ -7,7 +7,7 @@
 <head>
 	<meta name="description" content="A general practice website">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
-	<meta name=viewport content="width=device-width, initial-scale=1">
+	<meta name=viewport content="width=device-width, height=device-height, initial-scale=1">
 	<link rel="stylesheet" href="<?php echo base_url('styles/stylesheets/style.css'); ?>">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
